96d64fe1d775c4ac63f2e65cc968c8246580cec2
SnePatel <46272377+SnePatel@users.noreply.github.com>
2021-07-12 12:21:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(keys.mongoURI, {
 
 require("./routes/bookRoutes")(app);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
